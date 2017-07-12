c87149f03bd344a38b3151a6d033d37ca44e82be
lexiht <hazukitran@gmail.com>
2017-07-12 15:37:33 +0100

    Extract the logic of converting arguments to integers into private normalise method in Input.parse

--- lib/input.rb
+++ lib/input.rb
@@ -1,13 +1,23 @@
 class Input
-  def self.parse(file_path)
-    return puts "please provide correct file" if file_path.nil? || !File.exists?(file_path)
-    commands = {}
-    File.open(file_path).each do |line|
-      arguments = line.strip.split(' ')
-      command = arguments.shift
-      arguments = arguments.map { |value| value =~ /\d+/ ? value.to_i : value }
-      commands[command] = arguments
+  class << self
+    def parse(file_path)
+      return puts "please provide correct file" if file_path.nil? || !File.exists?(file_path)
+      commands = {}
+      File.open(file_path).each do |line|
+        arguments = line.strip.split(' ')
+        command = arguments.shift
+        arguments = normalise(arguments)
+        commands[command] = arguments
+      end
+      commands
     end
-    commands
+
+    private
+
+    def normalise(arguments)
+      arguments.map do |value|
+        value =~ /\d+/ ? value.to_i : value
+      end
+    end
   end
 end
