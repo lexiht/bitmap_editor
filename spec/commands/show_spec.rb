436f4eb78e6591f16dfaf34ed7564b13576776d0
lexiht <hazukitran@gmail.com>
2017-07-12 20:52:41 +0100

    Show 2x2 matrix on STDOUT

--- spec/commands/show_spec.rb
+++ spec/commands/show_spec.rb
@@ -3,9 +3,15 @@ require './lib/bitmap.rb'
 
 describe Show do
   describe '.apply' do
-    it 'output the 1x1 matrix' do
+    it 'outputs the 1x1 matrix' do
       bitmap = Bitmap.new(1, 1)
       message = "\"O\"\n"
+      expect { described_class.apply([], bitmap) }.to output(message).to_stdout
+    end
+
+    it 'outputs the 2x2 matrix' do
+      bitmap = Bitmap.new(2, 2)
+      message = "\"OO\"\n\"OO\"\n"
       expect { described_class.apply([], bitmap) }.to output(message).to_stdout
     end
   end
