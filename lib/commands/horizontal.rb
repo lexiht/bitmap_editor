38cadada9d1b8f8a3a09b1d82d41ac4209bd3ccb
lexiht <hazukitran@gmail.com>
2017-07-12 23:49:36 +0100

    Error handling for x out of range and x not integer in horizontal class

--- lib/commands/horizontal.rb
+++ lib/commands/horizontal.rb
@@ -1,15 +1,23 @@
 class Horizontal
   def self.apply(argument, bitmap)
-    column_min = argument[0] - 1
-    column_max = argument[1] - 1
-    row = argument[2] - 1
-    colour = argument[3]
-    selected_row = bitmap.matrix[row]
-    selected_row.each_with_index do |value, index|
-      if index >= column_min && index <= column_max
-        selected_row[index] = colour
+    if argument[0...-1].any? { |e| e.is_a?(String) }
+      puts 'Invalid coordinate'
+    else
+      column_min = argument[0] - 1
+      column_max = argument[1] - 1
+      if column_min < 0 || column_max + 1 > bitmap.column
+        puts 'The x coordinate is out of range'
+      else
+        row = argument[2] - 1
+        colour = argument[3]
+        selected_row = bitmap.matrix[row]
+        selected_row.each_with_index do |value, index|
+          if index >= column_min && index <= column_max
+            selected_row[index] = colour
+          end
+        end
+        bitmap
       end
     end
-    bitmap
   end
 end
--- spec/commands/horizontal_spec.rb
+++ spec/commands/horizontal_spec.rb
@@ -16,5 +16,17 @@ describe Horizontal do
       new_bitmap.matrix = [['O', 'O'], ['C', 'C']]
       expect(described_class.apply([1, 2, 2, 'C'], old_bitmap)).to eq(new_bitmap)
     end
+
+    it 'outputs error message when x is out of range' do
+      old_bitmap = Bitmap.new(2, 3)
+      message = "The x coordinate is out of range\n"
+      expect { described_class.apply([4, 5, 1, 'C'], old_bitmap) }.to output(message).to_stdout
+    end
+
+    it 'outputs error message when x coordinate is String' do
+      old_bitmap = Bitmap.new(2, 3)
+      message = "Invalid coordinate\n"
+      expect { described_class.apply([1, 'A', 'B', 'C'], old_bitmap) }.to output(message).to_stdout
+    end
   end
 end
