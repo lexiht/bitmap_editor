3afa3abe098069fed0ac0f95f486a8ddf68a4721
lexiht <hazukitran@gmail.com>
2017-07-12 18:01:39 +0100

    replace colour at coordinate 1, 2

--- lib/commands/colour.rb
+++ lib/commands/colour.rb
@@ -1,7 +1,7 @@
 class Colour
   def self.apply(argument, bitmap)
-    x = argument[0]
-    y = argument[1]
+    x = argument[0] - 1
+    y = argument[1] - 1
     colour = argument[2]
     bitmap.matrix[y][x] = colour
     bitmap
--- spec/commands/colour_spec.rb
+++ spec/commands/colour_spec.rb
@@ -4,10 +4,16 @@ describe Colour do
   describe '.apply' do
     it 'replace O to C at coordinate 1, 1' do
       old_matrix = Bitmap.new(1, 2)
-      old_matrix.matrix = [['O'], ['O']]
       new_matrix = Bitmap.new(1, 2)
       new_matrix.matrix = [['C'], ['O']]
       expect(described_class.apply([1, 1, 'C'], old_matrix)).to eq(new_matrix)
+    end
+
+    it 'replace O to C at coordinate 1, 2' do
+      old_matrix = Bitmap.new(1, 2)
+      new_matrix = Bitmap.new(1, 2)
+      new_matrix.matrix = [['O'], ['C']]
+      expect(described_class.apply([1, 2, 'C'], old_matrix)).to eq(new_matrix)
     end
   end
 end
