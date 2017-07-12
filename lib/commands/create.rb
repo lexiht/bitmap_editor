a41ac1210f15f6d285c020a17b55a574f7341097
lexiht <hazukitran@gmail.com>
2017-07-12 16:40:56 +0100

    When argument is an empty array, returns error message

--- lib/commands/create.rb
+++ lib/commands/create.rb
@@ -1,7 +1,11 @@
 class Create
   def self.apply(argument, bitmap)
-    width = argument[0]
-    height = argument[1]
-    Bitmap.new(width, height)
+    if argument.length != 2 || argument.any? { |e| e.is_a?(String) }
+      puts "Wrong input #{argument} after I"
+    else
+      width = argument[0]
+      height = argument[1]
+      Bitmap.new(width, height)
+    end
   end
 end
--- spec/commands/create_spec.rb
+++ spec/commands/create_spec.rb
@@ -3,13 +3,22 @@ require './lib/bitmap.rb'
 
 describe Create do
   describe '.apply' do
+
     it 'returns a 3x3 matrix' do
-      argument = [3, 3]
-      width = argument[0]
-      height = argument[1]
-      bitmap = nil
-      matrix = Bitmap.new(width, height)
-      expect(described_class.apply(argument, bitmap)).to eq(matrix)
+      matrix = Bitmap.new(3, 3)
+      expect(described_class.apply([3, 3], nil)).to eq(matrix)
+    end
+
+    it 'returns a 3x4 matrix' do
+      matrix = Bitmap.new(3, 4)
+      expect(described_class.apply([3, 4], nil)).to eq(matrix)
+    end
+
+    context 'wrong arguments' do
+      it 'returns error message when argument is an empty array' do
+        message = "Wrong input [] after I\n"
+        expect { described_class.apply([], nil) }.to output(message).to_stdout
+      end
     end
   end
 end
