689bbb5de6e904e13f079c0b0bdfbd8be7408502
lexiht <hazukitran@gmail.com>
2017-07-12 20:43:05 +0100

    Replace y range of O to C in 2x3 matrix

--- spec/commands/vertical_spec.rb
+++ spec/commands/vertical_spec.rb
@@ -9,5 +9,12 @@ describe Vertical do
       new_bitmap.matrix = [['O', 'C'], ['O', 'C']]
       expect(described_class.apply([2, 1, 2, 'C'], old_bitmap)).to eq(new_bitmap)
     end
+
+    it 'replace an y range of O to C in 2x3 matrix' do
+      old_bitmap = Bitmap.new(2, 3)
+      new_bitmap = Bitmap.new(2, 3)
+      new_bitmap.matrix = [['O', 'O'], ['C', 'O'], ['C', 'O']]
+      expect(described_class.apply([1, 2, 3, 'C'], old_bitmap)).to eq(new_bitmap)
+    end
   end
 end
