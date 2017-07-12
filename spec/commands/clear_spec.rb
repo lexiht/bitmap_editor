a052d17065525d7f8c464a241c5edfae97951137
lexiht <hazukitran@gmail.com>
2017-07-12 17:28:06 +0100

    reset 2x2 matrix when clear commands is being provided

--- spec/commands/clear_spec.rb
+++ spec/commands/clear_spec.rb
@@ -2,10 +2,17 @@ require './lib/commands/clear.rb'
 
 describe Clear do
   describe '.apply' do
-    it 'reset the matrix to original state' do
+    it 'reset the 1x1 matrix to original state' do
       old_matrix = Bitmap.new(1, 1)
       old_matrix.matrix = [['C']]
       new_matrix = Bitmap.new(1, 1).matrix
+      expect(described_class.apply([], old_matrix)).to eq(new_matrix)
+    end
+
+    it 'reset the 2x2 matrix to original state' do
+      old_matrix = Bitmap.new(2, 2)
+      old_matrix.matrix = [['C', 'C'], ['C', 'C'], ['C', 'C']]
+      new_matrix = Bitmap.new(2, 2).matrix
       expect(described_class.apply([], old_matrix)).to eq(new_matrix)
     end
   end
